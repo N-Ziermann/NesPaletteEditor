5a9e4b5e29e751e8bacf422229c1c239b7f5ec89
N-Ziermann <n-ziermann@protonmail.com>
2022-06-26 10:36:18 +0200

    Import react

--- src/components/PaletteEditor/index.jsx
+++ src/components/PaletteEditor/index.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import COLORS from "../../js/colors";
 import "./PaletteEditor.css";
 import classNames from "classnames";
